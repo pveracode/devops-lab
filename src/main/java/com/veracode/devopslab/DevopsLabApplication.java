04280834e31d40ceebfbeb3cbe463bef817863c4
pveracode <ptandon@veracode.com>
2026-07-09 12:20:04 +0530

    feat: initialize devops lab with Spring Boot and Docker

---
+++ src/main/java/com/veracode/devopslab/DevopsLabApplication.java
@@ -0,0 +1,13 @@
+package com.veracode.devopslab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/veracode/devopslab/DevopsLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veracode.devopslab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
